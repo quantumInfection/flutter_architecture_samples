ead6770ae59d2b42bfa57f09460193ca8184747a
Abdullah Aftab <a13dullah@Abdullahs-MacBook-Pro.local>
2019-05-19 02:45:55 +0500

    Fix deps

--- bloc_library/lib/blocs/simple_bloc_delegate.dart
+++ bloc_library/lib/blocs/simple_bloc_delegate.dart
@@ -7,15 +7,22 @@ import 'package:bloc/bloc.dart';
 // We can extend `BlocDelegate` and override `onTransition` and `onError`
 // in order to handle transitions and errors from all Blocs.
 class SimpleBlocDelegate extends BlocDelegate {
+
   @override
-  void onTransition(Transition transition) {
-    super.onTransition(transition);
-    print(transition);
+  void onEvent(Bloc bloc, Object event) {
+    print(bloc);
+    super.onEvent(bloc, event);
   }
 
   @override
-  void onError(Object error, StackTrace stacktrace) {
-    super.onError(error, stacktrace);
-    print(error);
+  void onError(Bloc bloc, Object error, StackTrace stacktrace) {
+    print(bloc);
+    super.onError(bloc, error, stacktrace);
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    print(bloc);
+    super.onTransition(bloc, transition);
   }
 }
